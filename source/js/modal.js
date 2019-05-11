25e0fa8ac04551ac46788692869676dacc16a32e
Svetlana177 <svetlanarozhkova177@mail.ru>
2019-05-11 08:58:10 +0300

    корректирует стили

--- source/js/modal.js
+++ source/js/modal.js
@@ -3,10 +3,8 @@ var link = document.querySelector(".catalog-button");
 var popup = document.querySelector(".modal__catalog");
 var close = popup.querySelector(".modal-close");
 
-
 var isStorageSupport = true;
 var storage = "";
-
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -16,16 +14,14 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
-
 });
 
 $('.catalog-button').on('click', function () {
     modal.style.display = "block";
   });
 
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
